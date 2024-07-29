aeceee992db42f8b53aa9355490c1c4ee234e762
AndersAskeland <anders.askeland@gmail.com>
2024-07-29 13:38:56 +0200

    Added documentation on dm_metabolic

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Demographic dataset for `{admiralmetabolic}`
+#'
+#' An example Demographic SDTM dataset for metabolic studies.
+#'
+#' @source Constructed using `dm` from the `{pharmaversesdtm}` package
+"dm_metabolic"
