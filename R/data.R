5ab300c9acaf693c1a7b55caefcaedab5e73c003
Anders Askeland <anders.askeland@gmail.com>
2024-07-30 10:05:59 +0200

    Closes #12 - Created DM metabolic dataset (#20)
    * Script to generate dm_metabolic dataset (#12)
    
    * Added documentation of dm_metabolic (#12)
    
    * Added usethis as suggests (#12)
    
    * Generated DM metabolic dataset (#12)
    
    * Ignore `data-raw` (#12)
    
    * Removed reference to admiralmetabolic (#12)
    
    The reference to admiralmetabolic caused an error when bulding the site with `pkgdown::build_site()`.
    
    * Fixed grammar (#12)
    
    Co-authored-by: Edoardo Mancini <53403957+manciniedoardo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Edoardo Mancini <53403957+manciniedoardo@users.noreply.github.com>

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Example demographic dataset
+#'
+#' An example Demographic SDTM dataset for metabolic studies.
+#'
+#' @source Constructed using `dm` from the `{pharmaversesdtm}` package
+"dm_metabolic"
