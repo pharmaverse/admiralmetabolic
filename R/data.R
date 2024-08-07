aac35c02fd9003b6b30eb5b62a7929a2fca5c598
SPUJ (Siddhesh Pujari) <spuj@novonordisk.com>
2024-08-07 07:41:19 +0200

    (#11): Added documentation for vs_metabolic

--- R/data.R
+++ R/data.R
@@ -4,3 +4,10 @@
 #'
 #' @source Constructed using `dm` from the `{pharmaversesdtm}` package
 "dm_metabolic"
+
+#' Example vital signs dataset
+#'
+#' An example Vital Signs SDTM dataset for metabolic studies.
+#'
+#' @source Constructed using `vs` from the `{pharmaversesdtm}` package and `dm_metabolic` from `{admiralmetabolic}` package
+"vs_metabolic"
