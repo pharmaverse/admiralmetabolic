e02efd4d42707bde7157430a29eaca7fed90a566
Edoardo Mancini <edoardo.mancini@roche.com>
2024-10-24 12:17:00 +0000

    #39 updated disclaimer based on review

--- data-raw/qs_metabolic.R
+++ data-raw/qs_metabolic.R
@@ -1,9 +1,9 @@
 #' Dataset: qs_metabolic
 #' Description: Create QS test SDTM dataset for metabolic studies,
 #' containing COEQ data (Control of Eating Questionnaire)
-#' Note that University  of Leeds are the copyright holders of the CoEQ and the test data included within
-#' `{admiralmetabolic}`is for not-for-profit use only. Any persons  or companies wanting to use the CoEQ
-#' should request a license to do so from the following
+#' Note: the University of Leeds are the copyright holders of the CoEQ and the test data included within {admiralmetabolic}
+#' is for not-for-profit use only within `{admiralmetabolic}` and `pharmaverse`-related examples/documentation. Any persons
+#' or companies wanting to use the CoEQ should request a license to do so from the following
 #' [link](https://licensing.leeds.ac.uk/product/control-of-eating-questionnaire-coeq).
 
 # Load libraries ----
