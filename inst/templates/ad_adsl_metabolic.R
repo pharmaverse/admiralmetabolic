d3501d30f9034ae595377090691daa0102537e51
Keita-Takahashi00 <keita.takahashi.4.biz@gmail.com>
2025-06-07 13:01:29 +0900

    Update inst/templates/ad_adsl_metabolic.R
    Co-authored-by: Andrii Yurovskyi <a.yurovskyi@icloud.com>

--- inst/templates/ad_adsl_metabolic.R
+++ inst/templates/ad_adsl_metabolic.R
@@ -170,7 +170,7 @@ adsl <- adsl %>%
   ) %>%
   derive_vars_merged(
     dataset_add = ds_ext,
-    by_vars = exprs(STUDYID, USUBJID),
+    by_vars = get_admiral_option("subject_keys"),
     new_vars = exprs(EOSDT = DSSTDT),
     filter_add = DSCAT == "DISPOSITION EVENT" & DSDECOD != "SCREEN FAILURE"
   ) %>%
