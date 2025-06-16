04d8b05acdaac468ac71db223a9ccf59fe27a6ad
Anders Askeland <anders.askeland@gmail.com>
2025-06-16 21:03:34 +0200

    Added data info

--- R/data.R
+++ R/data.R
@@ -7,3 +7,13 @@
 #' Derived from the `advs` template
 #'
 "admiralmetabolic_advs"
+
+#' Subject Level Analysis Dataset
+#'
+#' An example subject level analysis dataset
+#' @keywords datasets
+#' @family datasets
+#' @source
+#' Derived from the `ad_adsl_metabolic` template
+#'
+"admiralmetabolic_adsl"
